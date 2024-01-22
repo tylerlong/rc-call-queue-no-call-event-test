a08e5dfa914c12fe3e599a243e5f1712c69431fd
Tyler Liu <tyler4long@gmail.com>
2024-01-22 14:40:49 -0800

    Hello RC

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log(`Hello ${process.env.NAME}!`);
+import RingCentral from '@rc-ex/core';
+import WebSocketExtension from '@rc-ex/ws';
+
+const rc = new RingCentral({
+  clientId: process.env.RINGCENTRAL_CLIENT_ID!,
+  clientSecret: process.env.RINGCENTRAL_CLIENT_SECRET!,
+  server: process.env.RINGCENTRAL_SERVER_URL!,
+});
+
+const main = async () => {
+  await rc.authorize({
+    jwt: process.env.RINGCENTRAL_JWT_TOKEN!,
+  });
+  const wsExt = new WebSocketExtension();
+  await rc.installExtension(wsExt);
+  console.log(rc.token?.access_token);
+  await rc.revoke();
+};
+main();
